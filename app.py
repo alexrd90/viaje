6ec917c975dd4c1a784829f86a0029a6541bf1f4
unknown <alexrettig@hotmail.com>
2024-11-09 18:24:34 -0300

    Actualizar

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def logout():
 
 # Asegúrate de que el servidor está configurado para ejecutarse en el puerto deseado
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
